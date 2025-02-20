7a76bf9bf1e3d84d895467e5181bf59c1431504e
MustafaTahaCetin <=>
2025-02-20 23:18:48 +0300

    codes

---
+++ Form1.Designer.cs
@@ -0,0 +1,404 @@
+﻿namespace operatorler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Salmon;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(957, 158);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gabriola", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(272, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(397, 110);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mustafa Kitapçılık";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SandyBrown;
+            this.button1.Location = new System.Drawing.Point(261, 553);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Fiyat Hesaplama";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Salmon;
+            this.label2.Location = new System.Drawing.Point(25, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hikaye: 8TL";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.maskedTextBox4);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.maskedTextBox3);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.maskedTextBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(106, 203);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 340);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kitap Fiyatları";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(3, 295);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(167, 21);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "her ürün için geçerlidir.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(13, 273);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(316, 21);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "indirim tarfielerimiz yan tarafta yazmaktadır";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.DarkSalmon;
+            this.label8.Location = new System.Drawing.Point(177, 215);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 28);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Adet:";
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(247, 212);
+            this.maskedTextBox4.Mask = "00000";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(59, 36);
+            this.maskedTextBox4.TabIndex = 12;
+            this.maskedTextBox4.ValidatingType = typeof(int);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Salmon;
+            this.label9.Location = new System.Drawing.Point(25, 215);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(124, 28);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Gazete: 3TL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.DarkSalmon;
+            this.label6.Location = new System.Drawing.Point(177, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 28);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Adet:";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(247, 159);
+            this.maskedTextBox3.Mask = "00000";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(59, 36);
+            this.maskedTextBox3.TabIndex = 9;
+            this.maskedTextBox3.ValidatingType = typeof(int);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Salmon;
+            this.label7.Location = new System.Drawing.Point(39, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 28);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Öykü: 5TL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkSalmon;
+            this.label4.Location = new System.Drawing.Point(177, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Adet:";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(247, 108);
+            this.maskedTextBox2.Mask = "00000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(59, 36);
+            this.maskedTextBox2.TabIndex = 6;
+            this.maskedTextBox2.ValidatingType = typeof(int);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Salmon;
+            this.label5.Location = new System.Drawing.Point(11, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 28);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Roman: 18TL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkSalmon;
+            this.label3.Location = new System.Drawing.Point(177, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Adet:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(247, 57);
+            this.maskedTextBox1.Mask = "00000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(59, 36);
+            this.maskedTextBox1.TabIndex = 3;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label16);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.label17);
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Location = new System.Drawing.Point(494, 203);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(335, 340);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İndirim ve Fırsatlar";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.Color.GreenYellow;
+            this.label16.Location = new System.Drawing.Point(142, 271);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(54, 28);
+            this.label16.TabIndex = 19;
+            this.label16.Text = "%50";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.GreenYellow;
+            this.label14.Location = new System.Drawing.Point(140, 186);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(55, 28);
+            this.label14.TabIndex = 19;
+            this.label14.Text = "%40";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.Color.Salmon;
+            this.label17.Location = new System.Drawing.Point(48, 234);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(246, 28);
+            this.label17.TabIndex = 18;
+            this.label17.Text = "41 ve üzeri ürün alımında";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Salmon;
+            this.label15.Location = new System.Drawing.Point(46, 149);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(249, 28);
+            this.label15.TabIndex = 18;
+            this.label15.Text = "21-40 arası ürün alımında";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.GreenYellow;
+            this.label13.Location = new System.Drawing.Point(142, 97);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(55, 28);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "%20";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Salmon;
+            this.label12.Location = new System.Drawing.Point(48, 60);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(239, 28);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "0-20 arası ürün alımında";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.BackColor = System.Drawing.Color.Salmon;
+            this.label18.Location = new System.Drawing.Point(447, 553);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(261, 28);
+            this.label18.TabIndex = 20;
+            this.label18.Text = "İndirimsiz ödenecek tutar :";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.BackColor = System.Drawing.Color.GreenYellow;
+            this.label19.Location = new System.Drawing.Point(447, 595);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(253, 28);
+            this.label19.TabIndex = 20;
+            this.label19.Text = "İndirimle ödenecek tutar :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 664);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Kitapçı Dükkanı";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace operatorler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int Hadet, Hfiyat, Radet, Rfiyat, Oadet, Ofiyat, Gadet, Gfiyat, ToplamKitapAdedi;
+            double NormalFiyat, İndirimliFiyat;
+
+            Hfiyat = 8;
+            Rfiyat = 18;
+            Ofiyat = 5;
+            Gfiyat = 3;
+            // Değişkene atanmış fiyatlar
+
+            Hadet = Convert.ToInt32(maskedTextBox1.Text);
+            Radet = Convert.ToInt32(maskedTextBox2.Text);
+            Oadet = Convert.ToInt32(maskedTextBox3.Text);
+            Gadet = Convert.ToInt32(maskedTextBox4.Text);
+            // Adet girişlerini aldık
+
+            ToplamKitapAdedi = Hadet + Radet + Oadet + Gadet;
+            NormalFiyat = Hadet * Hfiyat + Radet * Rfiyat + Oadet * Ofiyat + Gadet * Gfiyat;
+            label18.Text = "İndirimsiz ödenecek tutar :" + NormalFiyat;
+            // Normalde ödenecek fiyatı bulduk
+
+            if (ToplamKitapAdedi <= 20 && ToplamKitapAdedi>0) //toplam adet yirmiden küçük veya eşitse
+            {
+                İndirimliFiyat = (NormalFiyat / 100) * 80; // %20 indirim
+                label19.Text = "İndirimle ödenecek tutar :" + İndirimliFiyat;
+            }
+            if (ToplamKitapAdedi >= 21 && ToplamKitapAdedi <= 40 )
+            {
+                İndirimliFiyat = (NormalFiyat / 100) * 60; // %40 indirim
+                label19.Text = "İndirimle ödenecek tutar :" + İndirimliFiyat;
+            }
+            if (ToplamKitapAdedi >= 41)
+            {
+                İndirimliFiyat = (NormalFiyat / 100) * 50; // %40 indirim
+                label19.Text = "İndirimle ödenecek tutar :" + İndirimliFiyat;
+            }
+            
+        }
+    }
+}
